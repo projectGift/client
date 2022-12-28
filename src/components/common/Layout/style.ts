468fa49b181b05a172fdde832f3cdfdb5a2a8cb6
zinukk <slsxpseh54@naver.com>
2022-12-28 15:07:02 +0900

    Modify : max-width 수정

--- src/components/common/Layout/style.ts
+++ src/components/common/Layout/style.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const LayoutWrapStyle = styled.div`
   margin: 0 auto;
-  max-width: 420px;
+  max-width: 430px;
   height: 100vh;
   border: 1px solid black;
 `;
