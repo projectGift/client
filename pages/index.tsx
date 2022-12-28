2aca23cdfacb33b36773485aad783fea9eca65e9
zinukk <slsxpseh54@naver.com>
2022-12-28 15:58:18 +0900

    와이어프레임 구현 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,84 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import styled from '@emotion/styled';
+import SEO from '@src/components/common/SEO';
+
 const Home = () => {
-  return <div>홈화면</div>;
+  const router = useRouter();
+
+  return (
+    <>
+      <SEO title="@@" />
+      <StHome>
+        <StHeader>로고</StHeader>
+        <StFooter>
+          <StWithoutLoginBtn
+            onClick={() => {
+              router.push(`/select`);
+            }}>
+            로그인 없이 이용하기
+          </StWithoutLoginBtn>
+          <StLoginBox>
+            {SOCIAL_LOGIN_TYPE.map(({ id, type }) => (
+              <StLoginBtn key={id}>{type}</StLoginBtn>
+            ))}
+          </StLoginBox>
+        </StFooter>
+      </StHome>
+    </>
+  );
 };
+
+const SOCIAL_LOGIN_TYPE = [
+  { id: 0, type: '카카오' },
+  { id: 1, type: '구글' },
+  { id: 2, type: '애플' },
+];
+
+const StHome = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const StHeader = styled.h1`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+const StFooter = styled.div`
+  position: absolute;
+  top: 80%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 80%;
+  height: 120px;
+`;
+
+const StWithoutLoginBtn = styled.button`
+  width: 100%;
+  height: 50px;
+`;
+
+const StLoginBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+`;
+
+const StLoginBtn = styled.button`
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
+`;
 
 export default Home;
