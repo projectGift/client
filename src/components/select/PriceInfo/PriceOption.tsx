aa58d9712fbd7aa3f85a886a47048232dc3dfcee
sol <solrasido55@gmail.com>
2023-01-09 17:09:02 +0900

    Refactor: PriceRange 로직 수정

--- src/components/select/PriceInfo/PriceOption.tsx
+++ src/components/select/PriceInfo/PriceOption.tsx
@@ -1,25 +1,21 @@
-import { SetStateAction, Dispatch, useState } from 'react';
+import { useState } from 'react';
 import styled from '@emotion/styled';
-import { useRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { priceState } from '@src/state/price';
 import Option from '@src/components/common/Option';
 
-interface IProps {
-  setPercent: Dispatch<SetStateAction<object>>;
-}
-
-const PriceOption = ({ setPercent }: IProps) => {
-  const [price, setPrice] = useRecoilState(priceState);
+const PriceOption = () => {
+  const setPrice = useSetRecoilState(priceState);
   const [selected, setSelected] = useState<string>('');
 
   const buttonHandler = (option: string) => {
     switch (option) {
       case '소소하게':
-        return setPrice({ start: 0, end: 100000 }), setPercent({ left: 0, right: 66.6 });
+        return setPrice({ start: 0, end: 100000 });
       case '적당하게':
-        return setPrice({ start: 100000, end: 200000 }), setPercent({ left: 33.3, right: 33.3 });
+        return setPrice({ start: 100000, end: 200000 });
       default:
-        return setPrice({ start: 200000, end: 300000 }), setPercent({ left: 66.6, right: 0 });
+        return setPrice({ start: 200000, end: 300000 });
     }
   };
 
--- src/state/price.ts
+++ src/state/price.ts
@@ -1,9 +1,17 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const priceState = atom<Iprice>({
   key: 'priceState',
   default: {
     start: 0,
     end: 300000,
+  },
+});
+
+export const percentState = selector({
+  key: 'percentState',
+  get: ({ get }) => {
+    const price: Iprice = get(priceState);
+    return { start: 100 - price.end / 3000, end: price.start / 3000 };
   },
 });
