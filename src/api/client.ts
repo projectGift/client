c498a3be7a59d5cb6d6adcd801b9ef665769692e
zinukk <slsxpseh54@naver.com>
2023-01-17 18:07:19 +0900

    ADD : axiosConfig

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-const baseURL: string = process.env.REACT_APP_BASE_URL as string;
+export const baseURL: string = process.env.NEXT_PUBLIC_BASE_URL as string;
 
 const client = axios.create({ baseURL });
 
+client.interceptors.response.use((res: AxiosResponse) => {
+  try {
+    if (res.status === 200) {
+      return res.data;
+    } else {
+      return [res.data.error, res.data.message];
+    }
+  } catch (e: any) {
+    return e.message;
+  }
+});
 export default client;
